cce401d92feb9a1364011a05a9aa372d2870b79a
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-07-25 21:13:40 +0100

    update style button to match ui

--- lib/styled_button.dart
+++ lib/styled_button.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class StyledButton extends StatelessWidget {
-  const StyledButton({required this.text, required this.onPressed, super.key});
+  const StyledButton(
+      {required this.child,
+      required this.onPressed,
+      super.key,
+      required String text});
 
-  final String text;
+  final Widget child;
   final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(onPressed: onPressed, child: Text(text));
+    return TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.brown.shade600,
+            foregroundColor: Colors.white,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)))),
+        onPressed: onPressed,
+        child: child);
   }
 }
