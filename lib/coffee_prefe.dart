a16675c2cfeb7cefdce202c97d061adceec8f1a4
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-07-25 21:13:25 +0100

    create function for button press

--- lib/coffee_prefe.dart
+++ lib/coffee_prefe.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_shop/small_body_test.dart';
 import 'package:coffee_shop/styled_body_text.dart';
+import 'package:coffee_shop/styled_button.dart';
 import 'package:flutter/material.dart';
 
 class CoffeePrefe extends StatefulWidget {
@@ -10,6 +11,21 @@ class CoffeePrefe extends StatefulWidget {
 }
 
 class _CoffeePrefeState extends State<CoffeePrefe> {
+  int strenght = 1;
+  int sugar = 1;
+
+  void updateStrenght() {
+    setState(() {
+      strenght = strenght < 5 ? strenght + 1 : 1;
+    });
+  }
+
+  void updateSugar() {
+    setState(() {
+      sugar = sugar < 5 ? sugar + 1 : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,32 +33,41 @@ class _CoffeePrefeState extends State<CoffeePrefe> {
         Row(
           children: [
             const SmallBodyTest('Strenght: '),
-            Image.asset(
-              'assets/img/coffee_bean.png',
-              width: 25,
-              color: Colors.brown.shade100,
-              colorBlendMode: BlendMode.multiply,
-            ),
-            const Expanded(child: SizedBox(width: 50)),
-            const SmallBodyTest("+")
+            for (int i = 0; i < strenght; i++)
+              Image.asset(
+                'assets/img/coffee_bean.png',
+                width: 25,
+                color: Colors.brown.shade100,
+                colorBlendMode: BlendMode.multiply,
+              ),
+            const Expanded(child: SizedBox()),
+            StyledButton(
+              onPressed: updateStrenght,
+              text: '+',
+              child: const Text("+"),
+            )
           ],
         ),
         const SizedBox(
           height: 10,
         ),
-        Row(
-          children: [
-            const SmallBodyTest('Sugar: '),
+        Row(children: [
+          const SmallBodyTest('Sugar: '),
+          if (sugar == 0) const SmallBodyTest("No Sugar"),
+          for (int i = 0; i < sugar; i++)
             Image.asset(
               'assets/img/sugar_cube.png',
               width: 25,
               color: Colors.brown.shade100,
               colorBlendMode: BlendMode.multiply,
             ),
-            const Expanded(child: SizedBox(width: 50)),
-            const SmallBodyTest("+")
-          ],
-        ),
+          const Expanded(child: SizedBox(width: 50)),
+          StyledButton(
+            onPressed: updateSugar,
+            text: '+',
+            child: const Text("+"),
+          )
+        ]),
       ],
     );
   }
